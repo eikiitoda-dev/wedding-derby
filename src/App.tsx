2dfed01cdd439735bfc3279110676310d4536f8a
itodaeiki <eikiitoda@gmail.com>
2026-08-02 00:16:26 +0900

    Create HomePage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
+import HomePage from "./pages/HomePage"
+
 function App() {
   return (
-    <div>
-      <h1>🏇 Wedding Derby</h1>
-      <p>結婚式レースゲーム</p>
-
-      <button>
-        ゲーム開始
-      </button>
-    </div>
+    <HomePage />
   )
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>🏇 Wedding Derby</h1>
+
+      <p>
+        結婚式レースゲーム
+      </p>
+
+      <button>
+        ゲーム開始
+      </button>
+    </div>
+  )
+}
+
+export default HomePage
